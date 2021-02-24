b5eb11692aa1c9d6913e339f239ecc40b01f2029
TheShy7 <tahmintalukder98@gmail.com>
2021-02-24 05:09:22 +0000

    added client events for when bot is ready and receives messages

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import discord
+
+client = discord.Client()
+
+# called when bot is ready to be used 
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+# called when bot receives a message
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+
+  if msg.content.startswith('$hello'):
+    await msg.channel.send('Hello!') 
