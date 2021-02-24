f46c2f71068de1f3c55911cd0641c7b2cef4f5aa
TheShy7 <tahmintalukder98@gmail.com>
2021-02-24 19:39:41 +0000

    bot responds with encouraging messages to sad messages from clients

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+sad_words = [
+  "sad", 
+  "depressed", 
+  "unhappy", 
+  "angry", 
+  "somber", 
+  "bitter", 
+  "pessimistic", 
+  "heartbroken", 
+  "miserable", 
+  "depressing"
+  ]
+
+starter_encouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are an amazing person!"
+]
 
 # makes request to API and parses returned JSON
 # for a random quote with the author
@@ -13,7 +33,7 @@ def get_quote():
   quote = json_data[0]['q'] + " - " + json_data[0]['a']
   return quote
 
-# called when bot is ready to be used 
+# called when bot is logged in and ready to be used 
 @client.event
 async def on_ready():
   print('Logged in as {0.user}'.format(client))
@@ -24,10 +44,22 @@ async def on_message(msg):
   if msg.author == client.user:
     return
 
-  if msg.content.startswith('$hello'):
+  msg_content = msg.content
+
+  if msg_content.startswith('$hello'):
+    # responds back with a "Hello!" after
+    # detecting the command 
     await msg.channel.send('Hello!')
-  elif msg.content.startswith('$inspire'):
+  elif msg_content.startswith('$inspire'):
+    # responds back with an inspirational quote 
+    # with its author after detecting the command 
     quote = get_quote()
     await msg.channel.send(quote)
+
+  # if bot detects any "sad" word from the 
+  # client's messages, then it will randomly respond with
+  # an encouraging message from the list
+  if any(word in msg_content for word in sad_words):
+    await msg.channel.send(random.choice(starter_encouragements))
 
 client.run(os.getenv('TOKEN'))
